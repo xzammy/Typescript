a53376ae7f1da04b4f6b5484c5d44d3c48da5f2d
Luo <cameron.luo@hpe.com>
2019-07-10 11:34:02 +0800

    Calculation, var(String) result

--- calculation.ts
+++ calculation.ts
@@ -3,3 +3,6 @@ function doCalculation(a,b,c){
 }
 var result = doCalculation (2,3,1);
 console.log('doCalculation(): ' + result);
+
+result = doCalculation("2","3","1");
+console.log('doCalculation(): '+ result);
