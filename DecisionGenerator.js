8538500159a85b4432cf28f41fb9dbb84134cbd2
Luo <xwhalex@icloud.com>
2019-07-22 11:58:17 +0800

    DecisionGenerator

---
+++ DecisionGenerator.js
@@ -0,0 +1,11 @@
+var phrases = [
+    "That sounds good",
+    "Yes you should definitely do that",
+    "I'm not sure if that's a great idea",
+    "Maybe not today",
+    "Computer says no."
+];
+
+//Should I have another bubble tea
+
+phrases[Math.floor(Math.random()*5)];
